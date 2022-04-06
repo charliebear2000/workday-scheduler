8351f8d177813f551c72cf9fe3c161aeca8332e8
Leah <amelia_rawls@ccpsnet.net>
2022-04-06 14:47:05 -0400

    add an activity to schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,22 @@
 $("#currentDay").html(moment().format("dddd, MMMM Do YYYY"));
 
-// saves the task in local storage
-var saveTasks = function() {
-   localStorage.setItem("tasks", JSON.stringify(tasks));
- };
+$(document).ready(function() {
 
- // task text was clicked
-$(".description").on("click", "p", function() {
-   console.log("<p> was clicked");
-   // get current text of p element
-   var text = $(this)
-     .text()
-     .trim();
- 
-   // replace p element with a new textarea
-   var textInput = $("<textarea>").addClass("form-control").val(text);
-   $(this).replaceWith(textInput);
- 
-   // auto focus new element
-   textInput.trigger("focus");
- });
- 
- // editable field was un-focused
- $(".description").on("blur", "textarea", function() {
-   // get current value of textarea
-   var text = $(this).val();
- 
-   // get status type and position in the list
-   var status = $(this)
-     .closest(".description")
-     .attr("id")
-     .replace("list-", "");
-   var index = $(this)
-     .closest(".list-group-item")
-     .index();
- 
-   // update task in array and re-save to localstorage
-   tasks[status][index].text = text;
-   saveTasks();
- 
-   // recreate p element
-   var taskP = $("<p>")
-     .addClass("m-1")
-     .text(text);
- 
-   // replace textarea with new content
-   $(this).replaceWith(taskP);
- });
- 
+  $(".saveBtn").on("click", function() {
+    var activity = $(this).siblings(".description").val();
+    var timeOfDay = $(this).parent().attr("id");
+    localStorage.setItem(timeOfDay, activity);
+    })
+
+
+$("#hourOne.description").val(localStorage.getItem("hourOne"));
+$("#hourTwo.description").val(localStorage.getItem("hourTwo"));
+$("#hourThree.description").val(localStorage.getItem("hourThree"));
+$("#hourFour.description").val(localStorage.getItem("hourFour"));
+$("#hourFive.description").val(localStorage.getItem("hourFive"));
+$("#hourSix.description").val(localStorage.getItem("hourSix"));
+$("#hourSeven.description").val(localStorage.getItem("hourSeven"));
+$("#hourEight.description").val(localStorage.getItem("hourEight"));
+$("#hourNine.description").val(localStorage.getItem("hourNine"));
+
+  })
